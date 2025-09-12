420bf56e9953000b56db472c27b9a6da714a9e00
Lucas Adda <addaboy13@gmail.com>
2025-09-11 22:32:09 -0400

    Week 2 Final

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -24,14 +24,24 @@ void draw()
             // Ground
             DrawRectangle(0, 700, 1200, 100, GREEN);
             // Text Box
-            DrawRectangle(40, 40, 180, 100, BLACK);
+            DrawRectangle(20, 30, 280, 100, BLACK);
+            DrawRectangle(320, 30, 280, 100, BLACK);
+            DrawRectangle(620, 30, 280, 100, BLACK);
+            // Velocity Calculation
+            float rad = launchAngle * DEG2RAD;
+            Vector2 velocity = { launchSpeed * cosf(rad), -launchSpeed * sinf(rad) };
+            // Creating Line
+            DrawLineEx(launchPos, Vector2{ launchPos.x + velocity.x, launchPos.y + velocity.y }, 7, RED);
             // Text (In the text box)
-            DrawText(TextFormat("Launch Position"), 42, 42, 30, WHITE);
+            DrawText("Launch Position", 42, 42, 30, WHITE);
             DrawText(TextFormat("(%.0f, %.0f)", launchPos.x, launchPos.y), 42, 82, 30, WHITE);
+            DrawText("Launch Angle", 342, 42, 30, WHITE);
+            DrawText(TextFormat("(%.1f Degrees)", launchAngle), 342, 82, 30, WHITE);
+            DrawText("Launch Speed", 642, 42, 30, WHITE);
+            DrawText(TextFormat("(%.1f)", launchSpeed), 642, 82, 30, WHITE);
             // Start Position
-            launchPos = { 200.0f, 500.0f };
             DrawCircleV(launchPos, 10, RED);
-            
+
         EndDrawing();
 }
 
@@ -39,6 +49,10 @@ int main()
 {
     InitWindow(InitialWidth, InitialHeight, "Lucas Adda 101566961 2005 Week 2");
     SetTargetFPS(TARGET_FPS);
+
+    launchPos = { 200.0f, 500.0f };
+    launchAngle = 50.0f;
+    launchSpeed = 400.0f;
 
     while (!WindowShouldClose()) // Loops TARGET_FPS per second
     {
