81af6f7b9366a743c21a9e25329e6232c3e9be49
Lucas Adda <addaboy13@gmail.com>
2025-10-16 19:48:38 -0400

    Started creating halfspace

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -25,38 +25,113 @@ float launchAngle;
 float launchSpeed;
 float rad;
 
+enum PhysicsShape
+{
+    CIRCLE,
+    HALF_SPACE
+};
+
 class PhysicsBody
 {
 public:
-    Vector2 launchStart {};
+    bool isStatic = false; // If this is set to true, don't move the object according to velocity or gravity
+    Vector2 position {};
     Vector2 projectileVelo {};
+    Color color = GREEN;
 
     // Projectile Specific Physics
     void update(float dt, Vector2 gravity)
     {
-        launchStart += projectileVelo * dt;
+        if (isStatic) return;
+
+        position += projectileVelo * dt;
         projectileVelo += gravity * dt;
     }
 
     virtual void draw() {}
+
+    virtual PhysicsShape Shape() = 0;
 };
 
 class PhysicsCircle : public PhysicsBody
 {
 public:
     float radius = 30.0f;
-    Color circleColor = GREEN;
     void draw() override
     {
-        DrawCircleV(launchStart, radius, circleColor);
+        DrawCircleV(position, radius, color);
+    }
+
+    PhysicsShape Shape() override
+    {
+        return CIRCLE;
     }
 };
 
-std::vector<PhysicsBody*> bird;
+class PhysicsHalfspace : public PhysicsBody
+{
+    // distance to the normal hints: a dot b * (b/||b||) ||b|| referring to magnitude
+    // dot product: a.x * b.x + a.y * b.y
+private:
+    float rotation = 0;
+    Vector2 normal = { 0, -1 };
+
+public:
+
+    void setRotationDegrees(float rotationInDeg)
+    {
+        rotation = rotationInDeg;
+        normal = Vector2Rotate({ 0, -1 }, rotation * DEG2RAD);
+    }
+
+    float getRotation()
+    {
+        return rotation;
+    }
+
+    Vector2 getNormal()
+    {
+        return normal;
+    }
+
+    void draw() override
+    {
+        DrawCircle(position.x, position.y, 8, RED);
+
+        DrawLineEx(position, position + normal * 30, 1, RED);
+
+        Vector2 parallelToSurface = Vector2Rotate(normal, PI * 0.5f);
+        DrawLineEx(position - parallelToSurface * 2000, position + parallelToSurface * 2000, 1, RED);
+    }
+
+    PhysicsShape Shape() override
+    {
+        return HALF_SPACE;
+    }
+};
+
+std::vector<PhysicsBody*> objects;
+PhysicsHalfspace halfspace;
+
+bool HalfspaceOverlap(PhysicsCircle* circle, PhysicsHalfspace* halfspace) 
+{
+    Vector2 displacementToCircle = circle->position - halfspace->position;
+    // Let d be the Dot Product of this displacement and the normal vector
+    // If D < 0, cricle is behind it and overlapping, if D > 0, circle is in front
+    // In other words... return (D < radius)
+    // return (Dot (displacement, normal) < radius)
+
+    float dot = Vector2DotProduct(displacementToCircle, halfspace->getNormal());
+    Vector2 vectorProjection = halfspace->getNormal() * dot;
+
+    DrawLineEx(circle->position, halfspace->position, 1, GRAY);
+
+    return false;
+}
 
 bool CircleOverlap(PhysicsCircle circleA, PhysicsCircle circleB)
 {
-    Vector2 displacement = circleB.launchStart - circleA.launchStart;
+    Vector2 displacement = circleB.position - circleA.position;
     float distance = Vector2Length(displacement);
     float sumOfRadii = circleA.radius + circleB.radius;
     if (sumOfRadii > distance)
@@ -69,22 +144,22 @@ bool CircleOverlap(PhysicsCircle circleA, PhysicsCircle circleB)
 
 void checkCollisions()
 {
-    for (int i = 0; i < bird.size(); i++) // Resets all circles to green if not touching
+    for (int i = 0; i < objects.size(); i++) // Resets all circles to green if not touching
     {
-        ((PhysicsCircle*)bird[i])->circleColor = GREEN;
+        ((PhysicsCircle*)objects[i])->color = GREEN;
     }
 
-    for (int i = 0; i < bird.size(); i++) // Overlap check
+    for (int i = 0; i < objects.size(); i++) // Overlap check
     {
-        for (int j = i + 1; j < bird.size(); j++)
+        for (int j = i + 1; j < objects.size(); j++)
         {
-            PhysicsCircle* circlePointerA = (PhysicsCircle*)bird[i];
-            PhysicsCircle* circlePointerB = (PhysicsCircle*)bird[j];
+            PhysicsCircle* circlePointerA = (PhysicsCircle*)objects[i];
+            PhysicsCircle* circlePointerB = (PhysicsCircle*)objects[j];
 
             if (CircleOverlap(*circlePointerA, *circlePointerB))
             {
-                circlePointerA->circleColor = RED;
-                circlePointerB->circleColor = RED;
+                circlePointerA->color = RED;
+                circlePointerB->color = RED;
             }
         }
     }
@@ -92,14 +167,14 @@ void checkCollisions()
 
 void cleanup()
 {
-    for (int i = 0; i < bird.size(); i++)
+    for (int i = 0; i < objects.size(); i++)
     {
-        if (bird[i]->launchStart.y > GetScreenHeight())
+        if (objects[i]->position.y > GetScreenHeight())
         {
-            auto iterator = (bird.begin() + i);
+            auto iterator = (objects.begin() + i);
             PhysicsBody* pointer = *iterator;
             delete pointer;
-            bird.erase(iterator);
+            objects.erase(iterator);
             i--;
         }
     }
@@ -126,18 +201,17 @@ void update()
     {
         // Creates and allocates memory for a new bird
         PhysicsCircle* newBird = new PhysicsCircle();
-        newBird->launchStart = launchPos;
+        newBird->position = launchPos;
         newBird->projectileVelo = velocity;
-        bird.push_back(newBird); 
+        objects.push_back(newBird); 
         // Adds a new bird to the list
     }
 
     cleanup();
-
     // Adds physics to all angry birds created
-    for (int i = 0; i < bird.size(); i++)
+    for (int i = 0; i < objects.size(); i++)
     {
-        bird[i]->update(dt, gravityAcceleration);
+        objects[i]->update(dt, gravityAcceleration);
     }
 
     checkCollisions();
@@ -153,6 +227,11 @@ void draw()
             GuiSliderBar(Rectangle{ 10, 150, 700, 20 }, "", TextFormat("Angle: %.2f", launchAngle), &launchAngle, 0, 180);
             GuiSliderBar(Rectangle{ 10, 190, 700, 20 }, "", TextFormat("Speed: %.2f", launchSpeed), &launchSpeed, 0, 500);
             GuiSliderBar(Rectangle{ 10, 230, 700, 20 }, "", TextFormat("Gravity: %.2f", gravityAcceleration.y), &gravityAcceleration.y, -350, 700);
+            GuiSliderBar(Rectangle{ 80, 270, 700, 20 }, "Halfspace X", TextFormat("%.0f", halfspace.position.x), &halfspace.position.x, 0, GetScreenWidth());
+            GuiSliderBar(Rectangle{ 80, 310, 700, 20 }, "Halfspace y", TextFormat("%.0f", halfspace.position.y), &halfspace.position.y, 0, GetScreenHeight());
+            float halfspaceRotation = halfspace.getRotation();
+            GuiSliderBar(Rectangle{ 110, 350, 500, 20 }, "Halfspace Rotate", TextFormat("%.0f", halfspaceRotation), &halfspaceRotation, -360, 360);
+            halfspace.setRotationDegrees(halfspaceRotation);
             // Ground
             DrawRectangle(0, 700, 1200, 100, DARKGREEN);
             // Text Box
@@ -164,8 +243,8 @@ void draw()
             velocity = { launchSpeed * cosf(rad), -launchSpeed * sinf(rad) };
             // Creating Line
             DrawLineEx(launchPos, Vector2{ launchPos + velocity }, 7, RED);
-            
-            DrawText(TextFormat("Objects: %i", bird.size()), 10, 270, 30, WHITE);
+            // Number of Birds Spawned Text
+            DrawText(TextFormat("Objects: %i", objects.size()), 10, 500, 30, WHITE);
             // Text (In the text box)
             DrawText("Launch Position", 32, 42, 30, WHITE);
             DrawText(TextFormat("(%.0f, %.0f)", launchPos.x, launchPos.y), 32, 82, 30, WHITE);
@@ -178,9 +257,9 @@ void draw()
             // Start Position
             DrawCircleV(launchPos, 10, RED);
             // Draws each bird in list
-            for (int i = 0; i < bird.size(); i++)
+            for (int i = 0; i < objects.size(); i++)
             {
-                bird[i]->draw();
+                objects[i]->draw();
             }
 
         EndDrawing();
@@ -188,8 +267,11 @@ void draw()
 
 int main()
 {
-    InitWindow(InitialWidth, InitialHeight, "Lucas Adda 101566961 2005 Week 3");
+    InitWindow(InitialWidth, InitialHeight, "Lucas Adda 101566961 2005 Week 5");
     SetTargetFPS(TARGET_FPS);
+    halfspace.isStatic = true;
+    halfspace.position = { 500, 700 };
+    objects.push_back(&halfspace);
 
     launchPos = { 200.0f, 700.0f };
     launchAngle = 50.0f;
